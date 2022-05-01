e4f676b1adcfc9c6394fb1c5444963fc91a2602c
siemen <siemensubbaiah1@gmail.com>
2022-05-02 01:10:26 +0530

    level 2 relation population

--- api/post/services/post.js
+++ api/post/services/post.js
@@ -1,8 +1,23 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  find(params, populate) {
+    return strapi
+      .query("post")
+      .find(params, [
+        "user",
+        "photo",
+        "likes",
+        "likes.user",
+        "likes.user.picture",
+        "comments",
+        "comments.user",
+        "comments.user.picture",
+      ]);
+  },
+};
