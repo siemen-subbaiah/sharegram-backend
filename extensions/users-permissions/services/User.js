08e3ac9faa1eed688b947b5e862ebc0fae13eb5d
siemen <siemensubbaiah1@gmail.com>
2022-05-03 21:47:39 +0530

    test saved

--- extensions/users-permissions/services/User.js
+++ extensions/users-permissions/services/User.js
@@ -2,6 +2,13 @@ module.exports = {
   fetchAuthenticatedUser(id) {
     return strapi
       .query("user", "users-permissions")
-      .findOne({ id }, ["picture", "following", "followers", "saveds"]);
+      .findOne({ id }, [
+        "picture",
+        "following",
+        "followers",
+        "saveds",
+        "saveds.post",
+        "saveds.post.photo",
+      ]);
   },
 };
